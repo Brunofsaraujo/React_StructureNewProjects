608c8ecd269222484ba0df9a865173957d6bf1c1
BrunoAraujo <bruno.araujo@uppertools.com.br>
2021-11-05 15:42:21 -0300

    React: Configuração source map

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
 export function App() {
+    throw new Error('qwe');
     return <h1>Hello World</h1>
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,42 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname, 'src', 'index.jsx'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
-    devServer: {
-        'static': {
-            directory: path.resolve(__dirname, 'public')
-        }
-    },    
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'public', 'index.html')
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                use: 'babel-loader',
-            }
-        ],
-    }
+	mode: "development",
+	devtool: false,
+	entry: path.resolve(__dirname, "src", "index.jsx"),
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js",
+	},
+	resolve: {
+		extensions: [".js", ".jsx"],
+	},
+	devServer: {
+		static: {
+			directory: path.resolve(__dirname, "public"),
+		},
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "public", "index.html"),
+		}),
+		new webpack.SourceMapDevToolPlugin({
+			test: /\.s?[ac]ss(\?hash=[A-z0-9]*)?$/,
+			columns: true,
+		}),
+		new webpack.EvalSourceMapDevToolPlugin({
+			test: /\.([jt]sx?)$/,
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.jsx$/,
+				exclude: /node_modules/,
+				use: "babel-loader",
+			},
+		],
+	},
 };
